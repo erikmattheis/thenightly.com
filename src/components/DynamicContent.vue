2c55e1fe9f08136d03c8b0b48ca8e7efdd376015
Erik Mattheis <erik@mattheis.org>
2023-11-29 05:51:58 -0600

    stop

--- src/components/DynamicContent.vue
+++ src/components/DynamicContent.vue
@@ -1,31 +1,29 @@
 <template>
-  <div>
-    <div>
-      <div v-if="editMode">
-        <form @submit.prevent="submitForm(article)">
-          <label>
-            Title
-            <input type="text" v-model="article.title" />
-          </label><br>
-          <label>
-            Short Title
-            <input type="text" v-model="article.shortTitle" />
-          </label><br>
-          <textarea v-model="article.content"></textarea><br>
-          <button type="submit">Submit</button><br>
-          <button type="button" @click="editMode = false">Cancel</button>
-        </form>
-      </div>
-      <div v-else>
-        <button @click="editMode = true">Edit</button>
-        <h2 style="color:red">{{ article.title }}</h2>
-        <p style="color:green" v-html="`<div>${article.description}</div>`"></p>
-        <!-- modern way without float to embed image with text wrapped around it -->
-
-        <p style="color:blue" v-html="`<div>${content}</div>`"></p>
-      </div>
+  <article>
+    <div v-if="editMode">
+      <form @submit.prevent="submitForm(article)">
+        <label>
+          Title
+          <input type="text" v-model="article.title" />
+        </label><br>
+        <label>
+          Short Title
+          <input type="text" v-model="article.shortTitle" />
+        </label><br>
+        <textarea v-model="article.content"></textarea><br>
+        <button type="submit">Submit</button><br>
+        <button type="button" @click="editMode = false">Cancel</button>
+      </form>
     </div>
-  </div>
+    <div v-else>
+      <!--<button @click="editMode = true">Edit</button>-->
+      <h2 style="color:red">{{ article.title }}</h2>
+      <p style="color:green" v-html="`<div>${article.description}</div>`"></p>
+      <!-- modern way without float to embed image with text wrapped around it -->
+
+      <p style="color:blue" v-html="`<div>${content}</div>`"></p>
+    </div>
+  </article>
 </template>
 
 <script>
@@ -69,3 +67,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+article {
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
--- src/components/TopicList.vue
+++ src/components/TopicList.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
 
-    <ul v-for="article in topics" :key="article.shortTitle">
-
-      <li v-if="editMode">
+    <ul v-if="editMode">
+      <li v-for="article in topics" :key="article.shortTitle">
 
         <form @submit.prevent="submitForm(article)">
 
@@ -17,19 +16,15 @@
           <button type="button" @click="editMode = false">Cancel</button>
 
         </form>
-
       </li>
+    </ul>
+    <ul v-else>
+      <li v-for="article in topics" :key="article.shortTitle" :style="{ 'background-color': article.color.background }">
 
-      <li v-else>
-
-        <button @click="editMode = true">
-          Edit
-        </button>
-
-        <router-link :to="{ name: 'DynamicContent', params: { topic: article.shortTitle } }">
+        <router-link :to="{ name: 'DynamicContent', params: { topic: article.shortTitle } }"
+          :style="{ 'background-color': article.color.background, 'color': article.color.color }" class="link">
           {{ article.shortTitle }}
         </router-link>
-
       </li>
 
     </ul>
@@ -52,6 +47,15 @@ export default {
       topics: dyes,
     };
   },
+  mounted() {
+    this.topics = this.topics.map((topic) => ({
+      ...topic,
+      color: {
+        background: topic.color,
+        color: this.contrastingColor(topic.color),
+      },
+    }));
+  },
   methods: {
     async submitForm(article) {
       try {
@@ -61,6 +65,20 @@ export default {
         console.error(error);
       }
     },
+    hexToRgb(hex) {
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      console.log('hextorgb', hex);
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      } : { r: 0, g: 0, b: 0 }; // default to black
+    },
+    contrastingColor(hex) {
+      const rgb = this.hexToRgb(hex);
+      const luminance = (0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b);
+      return (luminance < 140) ? '#ffffff' : '#000000';
+    },
   },
 };
 </script>
@@ -68,5 +86,22 @@ export default {
 <style scoped>
 button {
   margin-right: 0.5rem;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0;
+  padding: 1rem;
+}
+
+.link {
+
+  text-decoration: none;
+  font-weight: 600;
 }
 </style>
